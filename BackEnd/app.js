503a14ee1126c94cc519923ca15ed3876f3c9966
stivenkm <stivenkamilo@gmail.com>
2022-12-26 19:26:46 -0500

    PUERTO

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -32,7 +32,7 @@ mongoose.connect(
 app.use(express.json());
 app.use(
   Cors({
-    origin: process.env.APP_URL+"3000",
+    origin: process.env.APP_URL,
     // "http://localhost:3000" //direccion front para permisos
   })
 );
